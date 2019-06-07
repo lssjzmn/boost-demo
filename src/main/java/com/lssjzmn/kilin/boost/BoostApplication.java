9041469a7613159e922a8b6b0662f5aee2227425
lssjzmn <1262683351@qq.com>
2019-06-07 14:24:34 +0800

    add spring cloud Eureka pom

--- src/main/java/com/lssjzmn/kilin/boost/BoostApplication.java
+++ src/main/java/com/lssjzmn/kilin/boost/BoostApplication.java
@@ -3,9 +3,11 @@ package com.lssjzmn.kilin.boost;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
+@EnableEurekaServer
 @SpringBootApplication
 @EnableCaching
 @ComponentScan(basePackages = "com.lssjzmn.kilin.boost")
